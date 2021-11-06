0c59b3e471b0ef47630115289f1486358713dfec
fp-joel-dsouza <joel.dsouza@financepeer.co>
2021-11-07 04:13:05 +0530

    made changes

--- model.js
+++ model.js
@@ -8,12 +8,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var NEWSAPI = require('newsapi');
-var firebase = require('firebase');
-var config = {
-    apiKey: "AIzaSyCkZ0zRdKzYP9PPL36mEoUsr7YPARyQ1vg",
-    databaseURL: "https://nodetest-95f46.firebaseio.com",
-};
-firebase.initializeApp(config);
 var newsapi = new NEWSAPI('620a0602eb5a4a05be03c5d6d4a25e10');
 class Model {
     constructor() {
@@ -50,52 +44,7 @@ class Model {
                 }
             }));
         });
-        this.registerUser = (email, password) => __awaiter(this, void 0, void 0, function* () {
-            return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
-                try {
-                    firebase.auth().createUserWithEmailAndPassword(email, password).then(function (response) {
-                        var database = firebase.database().ref('user');
-                        var user = database.push({
-                            email: email,
-                            uid: response.uid
-                        });
-                        resolve({ msg: "Registration succesful. Thanks for registering", status: true });
-                    }).catch(function (error) {
-                        var errorCode = error.code;
-                        var errorMessage = error.message;
-                        console.log(errorMessage);
-                        console.log(errorCode);
-                        resolve({ msg: errorMessage, status: false });
-                    });
-                }
-                catch (e) {
-                    reject(e);
-                }
-            }));
-        });
-        this.signInUser = (email, password) => __awaiter(this, void 0, void 0, function* () {
-            console.log(email);
-            return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
-                firebase.auth().signInWithEmailAndPassword(email, password).then(function (response) {
-                    firebase.database().ref('/user/').orderByChild("email").equalTo(email).once('value').then(function (snapshot) {
-                        var key;
-                        snapshot.forEach(function (childSnapshot) {
-                            key = childSnapshot.key;
-                        });
-                        console.log(snapshot.child(key).val());
-                        resolve({ data: snapshot.child(key).val(), status: true });
-                    });
-                }).catch(function (error) {
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    console.log(errorMessage);
-                    console.log(errorCode);
-                    resolve({ msg: errorMessage, status: false });
-                });
-            }));
-        });
     }
 }
 module.exports = Model;
-// export {};
 //# sourceMappingURL=model.js.map
